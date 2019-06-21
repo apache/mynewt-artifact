5b2c4f803ea2330ce8799c026fe4606a1ab679e6
Christopher Collins <ccollins@apache.org>
2019-06-21 11:53:27 -0700

    Add missing Apache licenses

--- errors/errors.go
+++ errors/errors.go
@@ -1,5 +1,24 @@
-// This is a thin wrapper over the `pkg/errors` package.  It decorates this
-// package with the following functionality:
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// This is a thin wrapper over the `pkg/errors` package.  It decorates the
+// base package with the following functionality:
 //
 // 1. Wrap and Wrapf produce an error with exactly one stack trace.  If the
 // wrapped error already contains a stack trace, these functions just append
